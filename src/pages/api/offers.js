6291a7d3e119831bbdf4e6fb7371609bed10dbaf
patrykomiotek <patryk.omiotek@gmail.com>
2021-06-13 16:52:34 +0200

    feat: api routes

---
+++ src/pages/api/offers.js
@@ -0,0 +1,68 @@
+export default (req, res) => {
+
+  // database
+  // REST, GraphQL
+  // files
+
+  res.status(200).json([{
+    "id": "1",
+    "name": "Six Suites, Old Town",
+    "street": "Kołodziejska 2",
+    "postal_code": "80-836",
+    "city": "Gdańsk",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/79172064.webp?k=9af8e5c75879a392d627595f8fad96d205e23ed264ec3744476b57805fa28731&o="
+    }
+    ,{
+    "id": "2",
+    "name": "Hotel Arche Geologiczna",
+    "street": "ul. Geologiczna 4",
+    "postal_code": "02-246",
+    "city": "Warszawa",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/192541890.webp?k=179376106102b1cde30c1b2fd7f220f1047d3015427af40d04e0a7bb1ecd16eb&o="
+    }, {
+    "id": "3",
+    "name": "Galeria Rondo Wiatraczna Apartments",
+    "street": "Grochowska 207",
+    "postal_code": "04-077",
+    "city": "Warszawa",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/164843364.webp?k=c9b83ddbec06f9602cd3acd75506a03bc5bf250384193109fe665682f780b3ce&o="
+    
+    },
+    {
+    "id": "4",
+    "name": "Hotel Miodowy Młyn",
+    "street": "Legionów 3",
+    "postal_code": "27-500",
+    "city": "Opatów",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/30873029.webp?k=dea5daa25f917081221d64f1e3cb8c0d1094c40dfc00b612b508973f597aba4e&o="
+    },
+    {
+    "id": "5",
+    "name": "AMBER Guest Rooms",
+    "street": "Plac Wszystkich Świętych 8",
+    "postal_code": "31-004",
+    "city": "Kraków",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/274304132.webp?k=d151179b66ea7b23c80cf64401691c210ec4aa242b8f286100c5a1473cacbed8&o="
+    },
+    {
+    "id": "6",
+    "name": "Gościnna Kamienica",
+    "street": "Sienkiewicza 24",
+    "postal_code": "15-092",
+    "city": "Białystok",
+    "country_code": "PL",
+    "country_name": "Polska",
+    "cover": "https://cf.bstatic.com/xdata/images/hotel/square90/61756322.webp?k=74a323679fa16fdd4401004718d90de826142140b3a46e9e205ada418da1efa1&o="
+    }
+    ]);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import Menu from '../components/Menu';
-
-export default function Home() {
+export default function Home({ offers }) {
   return (
     <div>
+      <h1>Offers</h1>
       <main className={styles.main}>
-
+        {offers && offers.map(( offer) =>
+          <div key={`offer-${offer.id}`} className={styles.offersContainer}>
+            <div className={styles.offerCover}>
+              <img src={offer.cover} alt="" />
+            </div>
+            <div className={styles.offerBox}>
+              <h3>{offer.name}</h3>
+              <p>{offer.street}, {offer.postal_code} {offer.city}</p>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3000/api/offers');
+  const data = await response.json();
+
+  return {
+    props: {
+      offers: data
+    }
+  }
 }
